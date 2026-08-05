e308a36167a3df1c77f86d75f331be61bd424478
taka070600538-tech <taka070600538@gmail.com>
2026-08-05 17:40:52 +0900

    feat: 権限とコンポーネントをAndroidManifestに登録

---
+++ app/src/main/java/com/taka0/callrecorder/SettingsActivity.kt
@@ -0,0 +1,10 @@
+package com.taka0.callrecorder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/taka0/callrecorder/TranscribeActivity.kt
@@ -0,0 +1,10 @@
+package com.taka0.callrecorder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TranscribeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
